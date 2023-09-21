47f9a1ea392ef80ff901acfa7327366fb1a6d15d
Izabel Nascimento <izabelyale@hotmail.com>
2023-09-21 01:33:35 -0300

    removed TODOs

--- src/main/java/com/izabel/localiza/LocalizaApplication.java
+++ src/main/java/com/izabel/localiza/LocalizaApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LocalizaApplication {
 
-    //TODO DTOS
-    //TODO DURATION
-    //TODO PUBLICAR NO HEROKU
     public static void main(String[] args) {
         SpringApplication.run(LocalizaApplication.class, args);
     }
